ff0f7715c63d18c84585e87eca90af697cd0baaf
marcusmaday <marcus.maday@gmail.com>
2019-11-04 20:52:42 -0600

    Update to more basic format and using discord.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var Discord = require('discord.io');
+var Discord = require('discord.js');
 var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
@@ -7,42 +7,37 @@ logger.add(new logger.transports.Console, {
 	colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-	token: auth.token,
-	autorun: true
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('GotNBot is ready to ROCK!');
 });
-bot.on('ready', function (evt) {
-	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(bot.username + ' - (' + bot.id + ')');
-});
-//Welcome message to new members
-bot.on("guildMemberAdd", member => {
-	console.log(member);
-  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
-  member.guild.channels.get("welcome").send(`"${member.user.username}" has joined this server`);
-	member.sendMessage("Welcome to the server friend!  Our rules are x, y, and z.  If you break them you will be permabanned immediately.")
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
+
+// Create an event listener for messages
+client.on('message', message => {
 	// Our bot needs to know if it will execute a command
 	// It will listen for messages that will start with `!`
-	if (message.substring(0, 1) == '!') {
-		var args = message.substring(1).split(' ');
+	if (message.content.substring(0, 1) == '!') {
+		var args = message.content.substring(1).split(' ');
 		var cmd = args[0];
 
 		args = args.splice(1);
 		switch(cmd) {
 			// !ping
 			case 'ping':
-				bot.sendMessage({
-					to: channelID,
-					message: 'Pong!'
-				});
+				message.channel.send('Pong!');
 				break;
-			case 'join':
-				bot.emit("guildMemberAdd", message.member)
+			case 'hi':
+				message.channel.send("Hello there " + message.member);
 				break;
 		}
 	}
 });
+
+client.login(auth.token);

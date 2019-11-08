3a8a749c655539b3afc43d945aab2d7d7c04c8f4
marcusmaday <marcus.maday@gmail.com>
2019-11-07 18:16:26 -0600

    hard-coded auth for now

--- app.js
+++ app.js
@@ -1,59 +1,42 @@
 const Discord = require('discord.js');
-const auth = require('./auth.json');
-var client = new Discord.Client();
+const client = new Discord.Client();
 
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+	colorize: true
+});
+logger.level = 'debug';
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  logger.info(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+// Create an event listener for messages
+client.on('message', message => {
+	// Our bot needs to know if it will execute a command
+	// It will listen for messages that will start with `!`
+	if (message.content.substring(0, 1) == '!') {
+		var args = message.content.substring(1).split(' ');
+		var cmd = args[0];
+
+		args = args.splice(1);
+		switch(cmd) {
+			// !ping
+			case 'ping':
+				message.channel.send('Pong!');
+				break;
+			case 'hi':
+				message.channel.send("Hello there " + message.member);
+				break;
+		}
+	}
 });
 
-console.log(auth.token);
-client.login(auth.token);
-
-// const Discord = require('discord.js');
-// const client = new Discord.Client();
-
-// var logger = require('winston');
-// var auth = require('./auth.json');
-// // Configure logger settings
-// logger.remove(logger.transports.Console);
-// logger.add(new logger.transports.Console, {
-// 	colorize: true
-// });
-// logger.level = 'debug';
-
-// /**
-//  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
-//  * received from Discord
-//  */
-// client.on('ready', () => {
-//   logger.info(`Logged in as ${client.user.tag}!`);
-// });
-
-// // Create an event listener for messages
-// client.on('message', message => {
-// 	// Our bot needs to know if it will execute a command
-// 	// It will listen for messages that will start with `!`
-// 	if (message.content.substring(0, 1) == '!') {
-// 		var args = message.content.substring(1).split(' ');
-// 		var cmd = args[0];
-
-// 		args = args.splice(1);
-// 		switch(cmd) {
-// 			// !ping
-// 			case 'ping':
-// 				message.channel.send('Pong!');
-// 				break;
-// 			case 'hi':
-// 				message.channel.send("Hello there " + message.member);
-// 				break;
-// 		}
-// 	}
-// });
-
-// client.login('NTU2NTU2Mjk5MTI3NjE5NTg2.D27dmg.LH0dqheTWCkmdy_o6XXZKxT4SzM');
+client.login('NTU2NTU2Mjk5MTI3NjE5NTg2.XcSyvA.NPNr_--i7ViEp-cXJI5zG571Uco');
